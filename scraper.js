b5d23b595983f6ebc0635438a00975e474374855
unviere <145258065+unviere@users.noreply.github.com>
2025-06-30 18:04:31 +0200

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -109,7 +109,7 @@ async function main() {
     // Add jam info as an extra top-level property
     const finalOutput = {
         games: result,
-        defaultInfo: defaultInfo 
+        defaultData: defaultInfo 
     };
 
     fs.writeFileSync("game_data.json", JSON.stringify(finalOutput, null, 2));
